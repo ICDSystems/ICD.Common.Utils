846df86b287f4e26c17dc81d9dacd7fca7cf2e69
Chris Cameron <chris.cameron@wedoresi.com>
2017-07-17 12:14:16 -0400

    Added test for EnumerableExtensions.Consolidate()

---
+++ ICD.Common.Utils.Tests/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿using ICD.Common.Utils.Extensions;
+using NUnit.Framework;
+using System.Linq;
+
+namespace ICD.Common.Utils.Tests_NetStandard.Extensions
+{
+	[TestFixture]
+    public sealed class EnumerableExtensionsTest
+    {
+		[Test]
+		public void ConsolidateTest()
+		{
+			string[] sequence = EnumerableExtensions.Consolidate(new string[] { "A", "B", "B", "C" }).ToArray();
+
+			Assert.AreEqual(3, sequence.Length, StringUtils.ArrayFormat(sequence));
+			Assert.AreEqual("A", sequence[0]);
+			Assert.AreEqual("B", sequence[1]);
+			Assert.AreEqual("C", sequence[2]);
+		}
+	}
+}
