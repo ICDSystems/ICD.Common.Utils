4801d453472759ef567fde4c48d9860c1965d111
jeffery.thompson <jeffery.thompson@wedoresi.com>
2017-07-18 09:58:55 -0400

    Added extensions to byte for getting and setting bits

---
+++ ICD.Common.Utils/Extensions/ByteExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace ICD.Common.Utils.Extensions
+{
+	public static class ByteExtensions
+	{
+		public static bool GetBit(this byte b, int n)
+		{
+			if (n > 7 || n < 0)
+				throw new ArgumentException();
+
+			return (b & (1 << n)) == (1 << n);
+		}
+
+		public static byte SetBit(this byte b, int n, bool v)
+		{
+			if (v)
+				return SetBitOn(b, n);
+			return SetBitOff(b, n);
+		}
+
+		public static byte SetBitOn(this byte b, int n)
+		{
+			if (n > 7 || n < 0)
+				throw new ArgumentException();
+
+			return (byte)(b | (1 << n));
+		}
+
+		public static byte SetBitOff(this byte b, int n)
+		{
+			if (n > 7 || n < 0)
+				throw new ArgumentException();
+
+			return (byte)(b & ~(1 << n));
+		}
+	}
+}
