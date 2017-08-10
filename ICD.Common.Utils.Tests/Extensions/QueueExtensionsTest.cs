be7126cb09a9aa335cab08de197babac44760d1f
Chris Cameron <chris.cameron@wedoresi.com>
2017-08-10 16:23:34 -0400

    Initial commit of QueueExtensionsTest and StringBuilderExtensionsTest

---
+++ ICD.Common.Utils.Tests/Extensions/QueueExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using ICD.Common.Utils.Extensions;
+
+namespace ICD.Common.Utils.Tests.Extensions
+{
+    [TestFixture]
+    public sealed class QueueExtensionsTest
+    {
+        [Test]
+        public void EnqueueRangeTest()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.EnqueueRange(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(3, queue.Count);
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(3, queue.Dequeue());
+        }
+
+        [Test]
+        public void DequeueTest()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.EnqueueRange(new int[] { 1, 2, 3 });
+
+            int output;
+
+            Assert.AreEqual(3, queue.Count);
+            Assert.AreEqual(true, queue.Dequeue(out output));
+            Assert.AreEqual(1, output);
+
+            Assert.AreEqual(2, queue.Count);
+            Assert.AreEqual(true, queue.Dequeue(out output));
+            Assert.AreEqual(2, output);
+
+            Assert.AreEqual(1, queue.Count);
+            Assert.AreEqual(true, queue.Dequeue(out output));
+            Assert.AreEqual(3, output);
+
+            Assert.AreEqual(0, queue.Count);
+            Assert.AreEqual(false, queue.Dequeue(out output));
+            Assert.AreEqual(0, output);
+        }
+    }
+}
---
+++ ICD.Common.Utils.Tests/Extensions/StringBuilderExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Text;
+using ICD.Common.Utils.Extensions;
+
+namespace ICD.Common.Utils.Tests.Extensions
+{
+    [TestFixture]
+    public sealed class StringBuilderExtensionsTest
+    {
+        [Test]
+        public void ClearTest()
+        {
+            StringBuilder builder = new StringBuilder("test");
+            builder.Clear();
+
+            Assert.AreEqual(0, builder.Length);
+        }
+
+        [Test]
+        public void PopTest()
+        {
+            StringBuilder builder = new StringBuilder("test");
+
+            Assert.AreEqual("test", builder.Pop());
+            Assert.AreEqual(0, builder.Length);
+        }
+    }
+}
