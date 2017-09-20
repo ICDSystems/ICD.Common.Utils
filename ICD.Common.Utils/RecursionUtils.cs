bc61686fa8783ba7ef77fd1ea0d8f2a6f09e4265
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-20 11:05:54 -0400

    Initial commit of RecursionUtils

---
+++ ICD.Common.Utils/RecursionUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ICD.Common.Utils
+{
+	public static class RecursionUtils
+	{
+		/// <summary>
+		/// Returns all of the nodes in the tree via breadth-first search.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="root"></param>
+		/// <param name="getChildren"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> BreadthFirstSearch<T>(T root, Func<T, IEnumerable<T>> getChildren)
+		{
+			Queue<T> process = new Queue<T>();
+			process.Enqueue(root);
+
+			while (process.Count > 0)
+			{
+				T current = process.Dequeue();
+				yield return current;
+
+				foreach (T child in getChildren(current))
+					process.Enqueue(child);
+			}
+		}
+	}
+}
