0345543aba9245e8d00f5a2238e5ea33c7b25ab4
Chris Cameron <chris.cameron@wedoresi.com>
2017-09-30 14:55:32 -0400

    Initial commit of AssemblyExtensions

---
+++ ICD.Common.Utils/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+#if SIMPLSHARP
+using Crestron.SimplSharp.Reflection;
+#else
+using System.Reflection;
+#endif
+using ICD.Common.Properties;
+using ICD.Common.Utils.IO;
+
+namespace ICD.Common.Utils.Extensions
+{
+    public static class AssemblyExtensions
+    {
+		/// <summary>
+		/// Gets the path for the given assembly. Returns null if the assembly can not be found on disk.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		[CanBeNull]
+		public static string GetPath(this Assembly extends)
+		{
+			if (extends == null)
+				throw new ArgumentNullException();
+
+			string path = extends
+#if SIMPLSHARP
+				.GetName()
+#endif
+				.CodeBase;
+
+			if (path == null)
+			{
+				path = extends.Location;
+			}
+			else
+			{
+				const string prefix = @"file:///";
+				if (path.StartsWith(prefix))
+					path = path.Substring(prefix.Length);
+			}
+
+			return IcdFile.Exists(path) ? path : null;
+		}
+    }
+}
