4c5a2ab311eb3c707577ec05f1f16665f0877cb3
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-02 14:57:23 -0400

    Stub tests for ServiceProvider

---
+++ ICD.Common.Utils.Tests/Services/ServiceProviderTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace ICD.Common.Utils.Tests.Services
+{
+	[TestFixture]
+    public sealed class ServiceProviderTest
+    {
+		[Test]
+		public void GetServiceGenericTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void GetServiceTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void TryGetServiceGenericTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void TryGetServiceTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void AddServiceGenericTest()
+		{
+			Assert.Inconclusive();
+		}
+
+		[Test]
+		public void AddServiceTest()
+		{
+			Assert.Inconclusive();
+		}
+	}
+}
