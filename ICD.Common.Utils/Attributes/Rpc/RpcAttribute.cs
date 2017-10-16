57062276306dcbf79c6c29faa0c3ccd70fbc5675
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-16 16:41:08 -0400

    Fixing bad using directive

--- ICD.Common.Utils/Attributes/Rpc/RpcAttribute.cs
+++ ICD.Common.Utils/Attributes/Rpc/RpcAttribute.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Crestron.SimplSharp.Reflection;
 using ICD.Common.Attributes;
 using ICD.Common.Properties;
 using ICD.Common.Utils.Collections;
 using ICD.Common.Utils.Extensions;
 #if SIMPLSHARP
-
+using Crestron.SimplSharp.Reflection;
 #else
 using System.Reflection;
 #endif
