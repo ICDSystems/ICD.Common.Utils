076ed4dd9ef878ba369e0edb62c03bbd43d059a6
Chris Cameron <chris.cameron@wedoresi.com>
2018-01-05 10:35:14 -0500

    Updating copyright info

--- ICD.Common.Utils/Properties/AssemblyInfo.cs
+++ ICD.Common.Utils/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("ICD.Common.Utils")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Utils")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("1.0.1.*")]
