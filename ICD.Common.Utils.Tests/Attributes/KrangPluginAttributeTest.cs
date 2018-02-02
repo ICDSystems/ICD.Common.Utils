3a693d2e71a8b804287b96649619328496157c73
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-02 14:38:58 -0500

    Moving RpcAttribute into networking project

--- ICD.Common.Utils.Tests/Attributes/KrangPluginAttributeTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using NUnit.Framework;
-
-namespace ICD.Common.Utils.Tests.Attributes
-{
-	[TestFixture]
-    public sealed class KrangPluginAttributeTest : AbstractIcdAttributeTest
-    {
-    }
-}
