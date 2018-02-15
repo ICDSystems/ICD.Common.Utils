ee03f85704f9bfa20f8fb3455c6c9fecdf372368
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-15 16:34:25 -0500

    Removing bad token type check from AbstractGenericJsonConverter

---
+++ ICD.Common.Utils.Tests/Json/AbstractGenericJsonConverterTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ICD.Common.Utils.Tests.Json
+{
+    public abstract class AbstractGenericJsonConverterTest
+    {
+		[Test]
+	    public abstract void WriteJsonTest();
+
+		[Test]
+	    public abstract void ReadJsonTest();
+    }
+}
