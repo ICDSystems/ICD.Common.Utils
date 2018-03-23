6b23238162f6630dfb53d2cd1352db543b01dd2b
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-23 11:59:47 -0400

    Extension methods for getting upper and lower 4 bits in a byte

--- ICD.Common.Utils/Extensions/ByteExtensions.cs
+++ ICD.Common.Utils/Extensions/ByteExtensions.cs
@@ -7,22 +7,20 @@ namespace ICD.Common.Utils.Extensions
 		public static bool GetBit(this byte b, int n)
 		{
 			if (n > 7 || n < 0)
-				throw new ArgumentException();
+				throw new ArgumentOutOfRangeException("n");
 
 			return (b & (1 << n)) == (1 << n);
 		}
 
 		public static byte SetBit(this byte b, int n, bool v)
 		{
-			if (v)
-				return SetBitOn(b, n);
-			return SetBitOff(b, n);
+			return v ? SetBitOn(b, n) : SetBitOff(b, n);
 		}
 
 		public static byte SetBitOn(this byte b, int n)
 		{
 			if (n > 7 || n < 0)
-				throw new ArgumentException();
+				throw new ArgumentOutOfRangeException("n");
 
 			return (byte)(b | (1 << n));
 		}
@@ -30,9 +28,19 @@ namespace ICD.Common.Utils.Extensions
 		public static byte SetBitOff(this byte b, int n)
 		{
 			if (n > 7 || n < 0)
-				throw new ArgumentException();
+				throw new ArgumentOutOfRangeException("n");
 
 			return (byte)(b & ~(1 << n));
+		}
+
+		public static byte GetLower4Bits(this byte b)
+		{
+			return (byte)(b & 15);
+		}
+
+		public static byte GetUpper4Bits(this byte b)
+		{
+			return (byte)(b >> 4);
 		}
 	}
 }
