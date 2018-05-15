8da73593c441af7f17412abd152c1edbd9a8558a
Chris Cameron <chris.cameron@wedoresi.com>
2018-05-15 13:29:24 -0400

    feat: Initial commit of UriUtils

---
+++ ICD.Common.Utils/UriUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ICD.Common.Utils
+{
+	public static class UriUtils
+	{
+		/// <summary>
+		/// Attempts to parse the given URI string into a System.Uri instance.
+		/// </summary>
+		/// <param name="uri"></param>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public static bool TryParse(string uri, out Uri output)
+		{
+			output = null;
+
+			if (string.IsNullOrEmpty(uri))
+				return false;
+
+			try
+			{
+				output = new Uri(uri);
+				return true;
+			}
+			catch (UriFormatException)
+			{
+				return false;
+			}
+		}
+	}
+}
