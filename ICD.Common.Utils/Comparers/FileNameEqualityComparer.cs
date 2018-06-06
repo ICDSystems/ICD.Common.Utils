6ce52b74ac8273a04cceb079e9f68f922e2bd168
Chris Cameron <chris.cameron@wedoresi.com>
2018-06-06 11:01:16 -0400

    feat: Moved FileNameEqualityComparer from Settings

---
+++ ICD.Common.Utils/Comparers/FileNameEqualityComparer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ICD.Common.Utils.IO;
+
+namespace ICD.Common.Utils.Comparers
+{
+	public sealed class FileNameEqualityComparer : IEqualityComparer<string>
+	{
+		private static FileNameEqualityComparer s_Instance;
+
+		public static FileNameEqualityComparer Instance
+		{
+			get { return s_Instance ?? (s_Instance = new FileNameEqualityComparer()); }
+		}
+
+		private FileNameEqualityComparer()
+		{
+		}
+
+		public bool Equals(string x, string y)
+		{
+			return GetHashCode(x) == GetHashCode(y);
+		}
+
+		public int GetHashCode(string obj)
+		{
+			return obj == null ? 0 : IcdPath.GetFileName(obj).GetHashCode();
+		}
+	}
+}
