928f8e5e0429f44a457d206ccecdced796435312
Jeffery Thompson <jeffery.thompson@wedoresi.com>
2018-07-11 16:35:08 -0400

    feat: first go at ActionSchedulerService

---
+++ ICD.Common.Utils/Services/Scheduler/IScheduledAction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ICD.Common.Utils.Services.Scheduler
+{
+	public interface IScheduledAction
+	{
+		/// <summary>
+		/// Raised when the scheduled run time has changed. The sender of the event must be the action itself
+		/// </summary>
+		event EventHandler OnScheduledRunTimeChanged;
+
+		/// <summary>
+		/// Gets the next time this action should be run
+		/// </summary>
+		DateTime NextRunTime { get; }
+
+		void Run();
+	}
+}
