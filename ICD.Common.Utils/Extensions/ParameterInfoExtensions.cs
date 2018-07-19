939f361b54738d5e3e1391a3bc8d1004f6ecdd67
Chris Cameron <chris.cameron@wedoresi.com>
2018-07-19 11:36:56 -0400

    feat: Cross-platform extension for determining if a ParameterInfo represents an "out" parameter

---
+++ ICD.Common.Utils/Extensions/ParameterInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+#if SIMPLSHARP
+using Crestron.SimplSharp.Reflection;
+#else
+using System.Reflection;
+#endif
+
+namespace ICD.Common.Utils.Extensions
+{
+	public static class ParameterInfoExtensions
+	{
+		/// <summary>
+		/// Returns true if the given parameter is an "out" parameter.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static bool GetIsOut(this ParameterInfo extends)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+#if SIMPLSHARP
+			return extends.Attributes.HasFlag(ParameterAttributes.Out);
+#else
+			return extends.IsOut;
+#endif
+		}
+	}
+}
