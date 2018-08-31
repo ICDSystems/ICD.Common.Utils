ed299b8d4fb1a7b77b1a0ca4ce8fb6aaedfa0761
Chris Cameron <chris.cameron@wedoresi.com>
2018-08-31 09:52:53 -0400

    feat: Initial commit of RegexUtils

---
+++ ICD.Common.Utils/RegexUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ICD.Common.Utils
+{
+	public static class RegexUtils
+	{
+		/// <summary>
+		/// Shim to perform Match and Matches in one call.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <param name="pattern"></param>
+		/// <param name="match"></param>
+		/// <returns></returns>
+		public static bool Matches(string input, string pattern, out Match match)
+		{
+			match = Regex.Match(input, pattern);
+			return match.Success;
+		}
+
+		/// <summary>
+		/// Shim to perform Match and Matches in one call.
+		/// </summary>
+		/// <param name="input"></param>
+		/// <param name="pattern"></param>
+		/// <param name="options"></param>
+		/// <param name="match"></param>
+		/// <returns></returns>
+		public static bool Matches(string input, string pattern, RegexOptions options, out Match match)
+		{
+			match = Regex.Match(input, pattern, options);
+			return match.Success;
+		}
+	}
+}
