c048c4fc655b939c91d1caaa5c5e1f41b2c5ba8f
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-09 10:09:40 -0500

    test: Adding test case for RemoveCharacters string extension method

--- ICD.Common.Utils.Tests/Extensions/StringExtensionsTest.cs
+++ ICD.Common.Utils.Tests/Extensions/StringExtensionsTest.cs
@@ -1,73 +1,75 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ICD.Common.Utils.Extensions;
 using NUnit.Framework;
 
 namespace ICD.Common.Utils.Tests.Extensions
 {
-    [TestFixture]
-    public sealed class StringExtensionsTest
-    {
-        [Test]
-        public void IndexOfTest()
-        {
-            string first;
-            Assert.AreEqual(5, "test1test3test2".IndexOf(new[] { "test2", "test3" }, out first));
-            Assert.AreEqual("test3", first);
-        }
+	[TestFixture]
+	public sealed class StringExtensionsTest
+	{
+		[Test]
+		public void IndexOfTest()
+		{
+			string first;
+			Assert.AreEqual(5, "test1test3test2".IndexOf(new[] {"test2", "test3"}, out first));
+			Assert.AreEqual("test3", first);
+		}
 
-        [TestCase(true, "12345", '1')]
-        [TestCase(false, "12345", '2')]
-        public void StartsWithTest(bool expected, string value, char character)
-        {
-            Assert.AreEqual(expected, value.StartsWith(character));
-        }
+		[TestCase(true, "12345", '1')]
+		[TestCase(false, "12345", '2')]
+		public void StartsWithTest(bool expected, string value, char character)
+		{
+			Assert.AreEqual(expected, value.StartsWith(character));
+		}
 
-        [TestCase(true, "12345", '5')]
-        [TestCase(false, "12345", '2')]
-        public void EndsWithTest(bool expected, string value, char character)
-        {
-            Assert.AreEqual(expected, value.EndsWith(character));
-        }
+		[TestCase(true, "12345", '5')]
+		[TestCase(false, "12345", '2')]
+		public void EndsWithTest(bool expected, string value, char character)
+		{
+			Assert.AreEqual(expected, value.EndsWith(character));
+		}
 
-        [Test]
-        public void SplitCharCountTest()
-        {
-            string[] split = "123-456-789-0".Split('-', 2).ToArray();
+		[Test]
+		public void SplitCharCountTest()
+		{
+			string[] split = "123-456-789-0".Split('-', 2).ToArray();
 
-            Assert.AreEqual(2, split.Length);
-            Assert.AreEqual("123", split[0]);
-            Assert.AreEqual("456-789-0", split[1]);
-        }
+			Assert.AreEqual(2, split.Length);
+			Assert.AreEqual("123", split[0]);
+			Assert.AreEqual("456-789-0", split[1]);
+		}
 
-        [Test]
-        public void SplitChunksizeTest()
-        {
-            string[] split = "1234567890".Split(3).ToArray();
+		[Test]
+		public void SplitChunksizeTest()
+		{
+			string[] split = "1234567890".Split(3).ToArray();
 
-            Assert.AreEqual(4, split.Length);
-            Assert.AreEqual("123", split[0]);
-            Assert.AreEqual("456", split[1]);
-            Assert.AreEqual("789", split[2]);
-            Assert.AreEqual("0", split[3]);
-        }
+			Assert.AreEqual(4, split.Length);
+			Assert.AreEqual("123", split[0]);
+			Assert.AreEqual("456", split[1]);
+			Assert.AreEqual("789", split[2]);
+			Assert.AreEqual("0", split[3]);
+		}
 
-        [TestCase("12345", "   12 3 4  \t 5\n")]
-        public void RemoveWhitespaceTest(string expected, string value)
-        {
-            Assert.AreEqual(expected, value.RemoveWhitespace());
-        }
+		[TestCase("12345", "   12 3 4  \t 5\n")]
+		public void RemoveWhitespaceTest(string expected, string value)
+		{
+			Assert.AreEqual(expected, value.RemoveWhitespace());
+		}
 
-        [Test]
-        public void RemoveCharactersTest()
-        {
-            Assert.AreEqual("13457890", "1234567890".Remove(new[] { '2', '6' }));
-        }
+		[TestCase("1234567890", new[] {'2', '6'}, "13457890")]
+		[TestCase("912529434324", new[] {'-', '(', ')', '.', '+'}, "912529434324")]
+		public void RemoveCharactersTest(string value, IEnumerable<char> characters, string expected)
+		{
+			Assert.AreEqual(expected, value.Remove(characters));
+		}
 
-        [TestCase(true, "27652")]
-        [TestCase(false, "a27652")]
-        public void IsNumericTest(bool expected, string value)
-        {
-            Assert.AreEqual(expected, value.IsNumeric());
-        }
-    }
+		[TestCase(true, "27652")]
+		[TestCase(false, "a27652")]
+		public void IsNumericTest(bool expected, string value)
+		{
+			Assert.AreEqual(expected, value.IsNumeric());
+		}
+	}
 }
