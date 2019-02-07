066c9f93a7b6ccd77b9416e0a02b51d4cf4468b3
Chris Cameron <chris.cameron@wedoresi.com>
2019-02-07 15:32:49 -0500

    chore: Updating AssemblyInfo

--- ICD.Common.Utils/Properties/AssemblyInfo.cs
+++ ICD.Common.Utils/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Utils")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Utils")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
 [assembly: AssemblyVersion("9.0.0.0")]
