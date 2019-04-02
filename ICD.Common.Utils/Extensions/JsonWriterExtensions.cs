b2ca43baf1b059a617c2d54b947dd052a83fc51b
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-02 16:37:18 -0400

    feat: Adding extension methods for writing properties to JSON

--- ICD.Common.Utils/Extensions/JsonWriterExtensions.cs
+++ ICD.Common.Utils/Extensions/JsonWriterExtensions.cs
@@ -8,27 +8,6 @@ namespace ICD.Common.Utils.Extensions
 	public static class JsonWriterExtensions
 	{
 		/// <summary>
-		/// Writes the type value.
-		/// </summary>
-		/// <param name="extends"></param>
-		/// <param name="type"></param>
-		[PublicAPI]
-		public static void WriteType(this JsonWriter extends, Type type)
-		{
-			if (extends == null)
-				throw new ArgumentNullException("extends");
-
-			if (type == null)
-			{
-				extends.WriteNull();
-				return;
-			}
-
-			string name = type.GetMinimalName();
-			extends.WriteValue(name);
-		}
-
-		/// <summary>
 		/// Serializes the given sequence of items to the writer.
 		/// </summary>
 		/// <typeparam name="TItem"></typeparam>
@@ -78,6 +57,102 @@ namespace ICD.Common.Utils.Extensions
 					write(extends, writer, item);
 			}
 			writer.WriteEndArray();
+		}
+
+		/// <summary>
+		/// Writes the type value.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="type"></param>
+		[PublicAPI]
+		public static void WriteType(this JsonWriter extends, Type type)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			if (type == null)
+			{
+				extends.WriteNull();
+				return;
+			}
+
+			string name = type.GetMinimalName();
+			extends.WriteValue(name);
+		}
+
+		/// <summary>
+		/// Writes the property name and value to the writer.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="propertyName"></param>
+		/// <param name="value"></param>
+		public static void WriteProperty(this JsonWriter extends, string propertyName, object value)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			extends.WritePropertyName(propertyName);
+			extends.WriteValue(value);
+		}
+
+		/// <summary>
+		/// Writes the property name and value to the writer.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="propertyName"></param>
+		/// <param name="value"></param>
+		public static void WriteProperty(this JsonWriter extends, string propertyName, string value)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			extends.WritePropertyName(propertyName);
+			extends.WriteValue(value);
+		}
+
+		/// <summary>
+		/// Writes the property name and value to the writer.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="propertyName"></param>
+		/// <param name="value"></param>
+		public static void WriteProperty(this JsonWriter extends, string propertyName, DateTime value)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			extends.WritePropertyName(propertyName);
+			extends.WriteValue(value);
+		}
+
+		/// <summary>
+		/// Writes the property name and value to the writer.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="propertyName"></param>
+		/// <param name="value"></param>
+		public static void WriteProperty(this JsonWriter extends, string propertyName, bool value)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			extends.WritePropertyName(propertyName);
+			extends.WriteValue(value);
+		}
+
+		/// <summary>
+		/// Writes the property name and value to the writer.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <param name="propertyName"></param>
+		/// <param name="value"></param>
+		public static void WriteProperty(this JsonWriter extends, string propertyName, int value)
+		{
+			if (extends == null)
+				throw new ArgumentNullException("extends");
+
+			extends.WritePropertyName(propertyName);
+			extends.WriteValue(value);
 		}
 	}
 }
