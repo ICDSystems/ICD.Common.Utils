829c24b6676dde1ed7cba55953d3190b8150e1a3
Drew Tingen <drew.tingen@icdsystems.com>
2019-04-09 15:46:10 -0400

    feat: Adding SPlusUtils to convert ushort's to int

---
+++ ICD.Common.Utils/SPlusUtils.cs
@@ -0,0 +1,20 @@
+﻿using ICD.Common.Properties;
+
+namespace ICD.Common.Utils
+{
+	[PublicAPI("S+")]
+	public static class SPlusUtils
+	{
+		/// <summary>
+		/// Convert two ushort's to an int
+		/// </summary>
+		/// <param name="lowWord">ushort for the least significant 16 bits</param>
+		/// <param name="highWord">ushort for the most significant 1 bits</param>
+		/// <returns></returns>
+		[PublicAPI("S+")]
+		public static int ConvertToInt(ushort lowWord, ushort highWord)
+		{
+			return (highWord << 16) + lowWord;
+		}
+	}
+}
