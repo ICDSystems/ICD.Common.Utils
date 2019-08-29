bb41d59a4bf1dce2b9294dd3a041b0a1228c6b90
Chris Cameron <chris.cameron@wedoresi.com>
2019-08-29 17:21:57 -0400

    feat: Adding methods for deserializing an XML array of items

---
+++ ICD.Common.Utils.Tests/Xml/IcdXmlConvertTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using ICD.Common.Utils.Xml;
+using NUnit.Framework;
+
+namespace ICD.Common.Utils.Tests.Xml
+{
+	[TestFixture]
+	public sealed class IcdXmlConvertTest
+	{
+		[Test]
+		public void DeserializeArrayGenericTest()
+		{
+			const string xml = @"<A>
+	<B>1</B>
+	<B>2</B>
+</A>";
+
+			using (IcdXmlReader reader = new IcdXmlReader(xml))
+			{
+				// Read to the first element
+				reader.Read();
+
+				int[] values = IcdXmlConvert.DeserializeArray<int>(reader).ToArray();
+
+				Assert.AreEqual(new[] {1, 2}, values);
+			}
+		}
+
+		[Test]
+		public void DeserializeArrayTest()
+		{
+			Assert.Inconclusive();
+		}
+	}
+}
