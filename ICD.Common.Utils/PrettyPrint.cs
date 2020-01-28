8d2321c350af1010fc17c0776c9d9294a2fb7600
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-28 13:36:00 -0500

    refactor: Removing redundant code

--- ICD.Common.Utils/PrettyPrint.cs
+++ ICD.Common.Utils/PrettyPrint.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ICD.Common.Properties;
+﻿using ICD.Common.Properties;
+using ICD.Common.Utils.Json;
 
 namespace ICD.Common.Utils
 {
@@ -13,72 +11,16 @@ namespace ICD.Common.Utils
 		#region Methods
 
 		[PublicAPI]
-		public static void PrintLine<TKey, TValue>(IDictionary<TKey, TValue> dictionary)
+		public static void PrintLine([CanBeNull] object value)
 		{
-			if (dictionary == null)
-				throw new ArgumentNullException("dictionary");
-
-			IcdConsole.PrintLine(ToString(dictionary));
+			string serial = ToString(value);
+			IcdConsole.PrintLine(serial);
 		}
 
 		[PublicAPI]
-		public static void PrintLine<T>(IEnumerable<T> sequence)
+		public static string ToString([CanBeNull] object value)
 		{
-			if (sequence == null)
-				throw new ArgumentNullException("sequence");
-
-			IcdConsole.PrintLine(ToString(sequence));
-		}
-
-		[PublicAPI]
-		public static string ToString<TKey, TValue>(IDictionary<TKey, TValue> dictionary)
-		{
-			if (dictionary == null)
-				throw new ArgumentNullException("dictionary");
-
-			StringBuilder builder = new StringBuilder();
-			builder.AppendLine("{");
-
-			foreach (KeyValuePair<TKey, TValue> kvp in dictionary)
-			{
-				builder.Append('\t');
-				builder.Append(ToString(kvp.Key));
-				builder.Append(" : ");
-				builder.Append(ToString(kvp.Value));
-				builder.Append(',');
-				builder.AppendLine();
-			}
-
-			builder.Append("}");
-			return builder.ToString();
-		}
-
-		[PublicAPI]
-		public static string ToString<T>(IEnumerable<T> sequence)
-		{
-			if (sequence == null)
-				throw new ArgumentNullException("sequence");
-
-			StringBuilder builder = new StringBuilder();
-			builder.AppendLine("[");
-
-			foreach (T item in sequence)
-			{
-				builder.Append('\t');
-				builder.Append(ToString(item));
-				builder.Append(',');
-				builder.AppendLine();
-			}
-
-			builder.Append("]");
-			return builder.ToString();
-		}
-
-		[PublicAPI]
-		public static string ToString<T>(T value)
-		{
-// ReSharper disable once CompareNonConstrainedGenericWithNull
-			return StringUtils.ToRepresentation(value == null ? null : value.ToString());
+			return JsonUtils.Format(value);
 		}
 
 		#endregion
