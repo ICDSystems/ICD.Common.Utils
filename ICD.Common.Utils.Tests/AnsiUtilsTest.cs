3f62e7e929ee7845dab7349553991b5042429620
Chris Cameron <chris.cameron@wedoresi.com>
2020-02-25 19:05:40 -0500

    refactor: Splitting ANSI into codes and texts, instead of converting straight to HTML

---
+++ ICD.Common.Utils.Tests/AnsiUtilsTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ICD.Common.Utils.Tests
+{
+	[TestFixture]
+	public sealed class AnsiUtilsTest
+	{
+		[Test]
+		public void ToSpansTest()
+		{
+			string ansi = "\x1b[30mblack\x1b[37mwhite\x1b[0mdefault";
+			AnsiSpan[] spans = AnsiUtils.ToSpans(ansi).ToArray();
+
+			Assert.AreEqual(3, spans.Length);
+
+			Assert.AreEqual("black", spans[0].Text);
+			Assert.AreEqual("30", spans[0].Code);
+
+			Assert.AreEqual("white", spans[1].Text);
+			Assert.AreEqual("37", spans[1].Code);
+
+			Assert.AreEqual("default", spans[2].Text);
+			Assert.AreEqual("0", spans[2].Code);
+		}
+	}
+}
--- ICD.Common.Utils/AnsiUtils.cs
+++ ICD.Common.Utils/AnsiUtils.cs
@@ -1,4 +1,7 @@
-﻿namespace ICD.Common.Utils
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ICD.Common.Utils
 {
 	public static class AnsiUtils
 	{
@@ -12,6 +15,42 @@
 		public const string COLOR_YELLOW_ON_RED_BACKGROUND = "\x1b[93;41m";
 
 		public const string CODE_RESET = "\x1b[0m";
+
+		/// <summary>
+		/// Matches ANSI escape codes, e.g. \x1b[31m and \x1b[30;1m
+		/// </summary>
+		private const string ANSI_PATTERN = "(?'match'\x01b\\[(?'code'[\\d;]+)m)";
+
+		/// <summary>
+		/// Matches ANSI escape codes to HTML styles.
+		/// Color values are taken from PuTTY.
+		/// </summary>
+		private static readonly Dictionary<string, string> s_PuttyColors =
+			new Dictionary<string, string>
+			{
+				{"30", "000000"}, // Black
+				{"31", "BB0000"}, // Red
+				{"32", "00BB00"}, // Green
+				{"33", "BBBB00"}, // Yellow
+				{"34", "0000BB"}, // Blue
+				{"35", "BB00BB"}, // Magenta
+				{"36", "00BBBB"}, // Cyan
+				{"37", "BBBBBB"}, // White
+
+				{"30;1", "555555"}, // Bright Black
+				{"31;1", "FF5555"}, // Bright Red
+				{"32;1", "55FF55"}, // Bright Green
+				{"33;1", "FFFF55"}, // Bright Yellow
+				{"34;1", "5555FF"}, // Bright Blue
+				{"35;1", "FF55FF"}, // Bright Magenta
+				{"36;1", "55FFFF"}, // Bright Cyan
+				{"37;1", "FFFFFF"}, // Bright White
+			};
+
+		/// <summary>
+		/// Gets the color map matching PuTTY.
+		/// </summary>
+		public static IDictionary<string, string> PuttyColors { get { return s_PuttyColors; } }
 
 		/// <summary>
 		/// Constructor.
@@ -36,6 +75,74 @@
 			data = string.IsNullOrEmpty(data) ? data : data.Replace("%", "%%");
 
 			return string.Format("{0}{1}{2}", code, data, CODE_RESET);
+		}
+
+		/// <summary>
+		/// Splits the given ANSI string into spans.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static IEnumerable<AnsiSpan> ToSpans(string data)
+		{
+			if (string.IsNullOrEmpty(data))
+				yield break;
+
+			Regex regex = new Regex(ANSI_PATTERN);
+			Match match = regex.Match(data);
+
+			// No matches
+			if (!match.Success)
+				yield return new AnsiSpan {Text = data};
+
+			// Find the spans
+			while (match.Success)
+			{
+				// Get the code
+				string code = match.Groups["code"].Value;
+
+				// Get the text
+				Match next = match.NextMatch();
+				int startIndex = match.Index + match.Length;
+				int endIndex = next.Success ? next.Index : data.Length;
+				string text = data.Substring(startIndex, endIndex - startIndex);
+
+				// Build the span
+				if (text.Length > 0)
+					yield return new AnsiSpan { Code = code, Text = text };
+
+				// Loop
+				match = next;
+			}
+		}
+
+		/// <summary>
+		/// Removes the bright suffix from the code if present, otherwise appends a bright suffix.
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns></returns>
+		public static string InvertBright(string code)
+		{
+			return code.EndsWith(";1")
+				? code.Substring(0, code.Length - 1)
+				: code + ";1";
+		}
+	}
+
+	public sealed class AnsiSpan
+	{
+		public string Code { get; set; }
+		public string Text { get; set; }
+
+		/// <summary>
+		/// Gets the color value for the code.
+		/// </summary>
+		/// <param name="colors"></param>
+		/// <param name="invertBright"></param>
+		/// <returns></returns>
+		public string GetColor(IDictionary<string, string> colors, bool invertBright)
+		{
+			string code = invertBright ? AnsiUtils.InvertBright(Code) : Code;
+			return colors[code];
 		}
 	}
 }
