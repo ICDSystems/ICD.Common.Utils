3da9b23e126f24248b8a758b7edf89c16953d636
Chris Cameron <chris.cameron@wedoresi.com>
2020-06-03 16:14:43 -0400

    feat: Adding simple IGenericEventArgs interface

--- ICD.Common.Utils/EventArguments/GenericEventArgs.cs
+++ ICD.Common.Utils/EventArguments/GenericEventArgs.cs
@@ -4,10 +4,17 @@ namespace ICD.Common.Utils.EventArguments
 {
 	public class GenericEventArgs<T> : EventArgs, IGenericEventArgs<T>
 	{
+		private readonly T m_Data;
+
 		/// <summary>
 		/// Gets the wrapped data associated with the event.
 		/// </summary>
-		public T Data { get; private set; }
+		object IGenericEventArgs.Data { get { return Data; } }
+
+		/// <summary>
+		/// Gets the wrapped data associated with the event.
+		/// </summary>
+		public T Data { get { return m_Data; } }
 
 		/// <summary>
 		/// Constructor.
@@ -15,7 +22,7 @@ namespace ICD.Common.Utils.EventArguments
 		/// <param name="data"></param>
 		public GenericEventArgs(T data)
 		{
-			Data = data;
+			m_Data = data;
 		}
 	}
 }
--- ICD.Common.Utils/EventArguments/IGenericEventArgs.cs
+++ ICD.Common.Utils/EventArguments/IGenericEventArgs.cs
@@ -1,10 +1,18 @@
 ﻿namespace ICD.Common.Utils.EventArguments
 {
-	public interface IGenericEventArgs<T>
+	public interface IGenericEventArgs
 	{
 		/// <summary>
 		/// Gets the wrapped data associated with the event.
 		/// </summary>
-		T Data { get; }
+		object Data { get; }
+	}
+
+	public interface IGenericEventArgs<T> : IGenericEventArgs
+	{
+		/// <summary>
+		/// Gets the wrapped data associated with the event.
+		/// </summary>
+		new T Data { get; }
 	}
 }
