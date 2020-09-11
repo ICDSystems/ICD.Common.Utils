9a68f0446d36da2696eda31e15d132efc3516ab2
Chris Cameron <chris.cameron@wedoresi.com>
2020-09-11 11:17:50 -0400

    test: Moved version comparer tests from settings

---
+++ ICD.Common.Utils.Tests/Comparers/UndefinedVersionComparerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ICD.Common.Utils.Comparers;
+using NUnit.Framework;
+
+namespace ICD.Common.Utils.Tests.Comparers
+{
+	[TestFixture]
+	public sealed class UndefinedVersionComparerTest
+	{
+		[Test]
+		public void CompareToTest()
+		{
+			Assert.AreEqual(0, UndefinedVersionComparer.Instance.Compare(new Version(0, 0), new Version(0, 0, 0, 0)));
+			Assert.AreEqual(-1, UndefinedVersionComparer.Instance.Compare(new Version(0, 0), new Version(1, 0, 0, 0)));
+			Assert.AreEqual(1, UndefinedVersionComparer.Instance.Compare(new Version(1, 0), new Version(0, 0, 0, 0)));
+		}
+	}
+}
---
+++ ICD.Common.Utils.Tests/Comparers/UndefinedVersionEqualityComparerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ICD.Common.Utils.Comparers;
+using NUnit.Framework;
+
+namespace ICD.Common.Utils.Tests.Comparers
+{
+	[TestFixture]
+	public sealed class UndefinedVersionEqualityComparerTest
+	{
+		[Test]
+		public void Equals()
+		{
+			Assert.IsTrue(UndefinedVersionEqualityComparer.Instance.Equals(new Version(0, 0), new Version(0, 0, 0, 0)));
+			Assert.IsFalse(UndefinedVersionEqualityComparer.Instance.Equals(new Version(0, 0), new Version(1, 0, 0, 0)));
+		}
+	}
+}
