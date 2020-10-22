2235eeeb9d7e526b8572bb8a101560c3302c23b9
Chris Cameron <chris.cameron@wedoresi.com>
2020-10-22 12:17:55 -0400

    feat: Initial commit of INotifyCollectionChanged

---
+++ ICD.Common.Utils/Collections/INotifyCollectionChanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ICD.Common.Utils.Collections
+{
+	// Poor-mans System.Collections.Specialized
+	// Delete when we finally drop Crestron 3-series.
+	public interface INotifyCollectionChanged
+	{
+		/// <summary>
+		/// Raised when the contents of the collection change, or the order changes.
+		/// </summary>
+		event EventHandler OnCollectionChanged;
+	}
+}
