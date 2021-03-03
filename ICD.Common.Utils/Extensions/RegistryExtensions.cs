d1f809693311761bd2b6c802a5397d09cbf16d5c
Chris Cameron <chris.cameron@wedoresi.com>
2021-03-03 16:45:19 -0500

    feat: Initial commit of RegistryExtensions

---
+++ ICD.Common.Utils/Extensions/RegistryExtensions.cs
@@ -0,0 +1,25 @@
+﻿#if !SIMPLSHARP
+using System.Collections.Generic;
+using System.Linq;
+using ICD.Common.Properties;
+using Microsoft.Win32;
+
+namespace ICD.Common.Utils.Extensions
+{
+	public static class RegistryExtensions
+	{
+		/// <summary>
+		/// Opens subkeys for the given registry key.
+		/// </summary>
+		/// <param name="extends"></param>
+		/// <returns></returns>
+		public static IEnumerable<RegistryKey> OpenSubKeys([NotNull] this RegistryKey extends)
+		{
+			return extends.GetSubKeyNames()
+			              .Select(extends.OpenSubKey)
+			              .Where(k => k != null);
+		}
+	}
+}
+
+#endif
