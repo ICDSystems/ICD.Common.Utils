4d84ded416918e9b10bd600927e47cd9faef8973
Scott Pidzarko <scottpidzarko@users.noreply.github.com>
2023-06-16 16:36:41 -0600

    chore: Update SafeMutex.cs to remove commented out boilerplate
    IDisposable boilerplate generates commented out finalizer. Removing commented out finalizer at request of upstream maintainer.

--- ICD.Common.Utils/Threading/SafeMutex.cs
+++ ICD.Common.Utils/Threading/SafeMutex.cs
@@ -113,14 +113,6 @@ namespace ICD.Common.Utils
                 		_disposedValue = true;
 			}
 		}
-        
-
-		// override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-		// ~SafeMutex()
-		// {
-		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-		//     Dispose(disposing: false);
-		// }
 
 		/// <inheritdoc/>
 		public void Dispose()
